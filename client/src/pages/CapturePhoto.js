fdb5a942b63324d6e1ab6bc2d9bea334b9d5bc39
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2019-12-14 12:55:08 -0800

    edit login page

--- client/src/pages/CapturePhoto.js
+++ client/src/pages/CapturePhoto.js
@@ -1,12 +1,18 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
+import Camera from "../components/Camera/Camera"
 
-// class CapturePhoto extends Component {
+class CapturePhoto extends Component {
 
-//   render() {
-//     return (
+  render() {
+    return (
+      <div>
+        <Camera />
+        <div>
+          <i className="far fa-circle"></i>
+        </div>
+      </div>
+    )
+  }
+}
 
-//     )
-//   }
-// }
-
-// export default CapturePhoto;
+export default CapturePhoto;
