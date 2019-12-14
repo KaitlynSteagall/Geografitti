88852c13b9f62cbe4738e4d52df56b65c513ace7
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-12-14 11:12:08 -0800

    under pages, there is now a login folder with code both in java and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login"
+import Login from "./pages/Login/Login"
 import Menu from "./pages/Menu"
 import TagArea from "./components/Canvas"; 
 
@@ -12,6 +12,8 @@ class App extends Component {
       <div className="App">
         <Menu /> {/* This will be converted to a route. Just using this to see work */}
         <TagArea />
+         {/* <Login />  {TO DO: make this into a route }   */}
+       
         
         
       </div>
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Login extends Component {
+
+  render() {
+    return (
+			<div className="container h-80">
+				<div className="row align-items-center h-100 ">
+          <div className="col-3 mx-auto">
+            <div className="text-center">
+            <img id="profile-img" class="img.avatar" src="./splash.jpg" alt="graffiti" />
+             <p id="profile-name" class="profile-name-card" />
+             <form  className="form-signin">							
+ 								<input type="username" name="username" id="inputUname" class="form-control form-group" placeholder="username" required autofocus />
+                <input type="password" name="password" id="inputPassword" class="form-control form-group" placeholder="password" required autofocus />
+								<button className="btn btn-primary" type="button" enabled>
+  							<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+									Enter
+									</button>
+										<button className="btn btn-primary" type="button" enabled>
+									<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+								Sign up
+								</button>
+								</form>
+							
+								
+						</div>
+					</div>
+				</div>
+			</div>
+    );
+  }
+}
+
+export default Login;
