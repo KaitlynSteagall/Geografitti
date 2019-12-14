6099e30f44fa8791b2928f2a6831406c6122f2b9
eazimms <eric.allen.zimmerman@gmail.com>
2019-12-14 10:33:28 -0800

    all updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Menu from "./pages/Menu"
+import TagArea from "./components/Canvas"; 
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Menu /> {/* This will be converted to a route. Just using this to see work */}
+        <TagArea />
+        
+        
       </div>
     );
   }
