fc4370698c26d47e2b0970b50495496bce095472
Mars Getsoian <m.getsoian@gmail.com>
2019-12-14 11:13:00 -0800

    usePosition helper script

--- client/src/components/WhereAmI/index.js
+++ client/src/components/WhereAmI/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {usePosition} from './usePosition';
-// usePosition is a pretty sophisticated system built into the browser that should, of its own accord, ask for user authorization to track, and also choose appropriate location methods (GPS/wifi/MAC) and throttle queries to a reasonable number so the client device doesn't lose battery too quickly or spam nearby towers
+// usePosition is sent over from a separate script
 
 // this particular component just makes a div that asks the device where it is and displays it to the user in text
 
