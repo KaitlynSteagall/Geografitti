67e9b8826983e0fdd4a372d993e80a0215129ade
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-12-16 10:11:27 -0800

    gallery code into portfolio page, placeholder images put into asset folder

---
+++ client/src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Gallery, GalleryImage } from "react-gesture-gallery";
+
+const images = [
+  "./assets/logincity.jpg",
+  "./assets/splash.jpg",
+  "./assets/image.png"
+];
+
+class Portfolio extends Component {
+  render() {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      if (index === 4) {
+        setIndex(0);
+      } else {
+        setIndex(prev => prev + 1);
+      }
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [index]);
+
+  return (
+    <Gallery
+      style={{
+        background: "transparent",
+        height: "60vh",
+        width: "60vw"
+      }}
+      index={index}
+      onRequestChange={i => {
+        setIndex(i);
+      }}
+    >
+      {images.map(image => (
+        <GalleryImage objectFit="contain" key={image} src={image} />
+      ))}
+    </Gallery>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Portfolio />, rootElement);
+
+export default Portfolio;
