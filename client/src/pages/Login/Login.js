099e06726e64f9206d968c695b8004127b491a5f
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-12-19 10:59:34 -0800

    modal working in login page, style sheet update, jquery dependency is now in json file

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,38 +1,79 @@
-import React, { Component } from "react";
-import "./style.css";
-import avatar from "../../assets/splash.jpg"
-import {Link, BrowserRouter as Router } from 'react-router-dom'
-
+/* eslint-disable react/jsx-no-duplicate-props */
+import React, { Component } from 'react'
+import './style.css'
+import $ from 'jquery'
 
 class Login extends Component {
+  componentDidMount () {
+    $('.log-in').click(function () {
+      $('.signIn').addClass('active-dx')
+      $('.signUp').addClass('inactive-sx')
+      $('.signUp').removeClass('active-sx')
+      $('.signIn').removeClass('inactive-dx')
+    })
 
-	render() {
-		return (
-			<div className="container h-80">
-				<div className="row align-items-center h-100 ">
-					<div className="col-3 mx-auto">
-						<div className="text-center">
-							<img id="profile-img" class="img.avatar" src={avatar} alt="graffiti" />
-							<p id="profile-name" class="profile-name-card" />
-							<form className="form-signin">
-								<input type="username" name="username" id="inputUname" class="form-control form-group" placeholder="username" required autofocus />
-								<input type="password" name="password" id="inputPassword" class="form-control form-group" placeholder="password" required autofocus />
-								<button className="btn btn-primary" type="button" enabled>
-									<span role="status" aria-hidden="true" /><Link to="/Menu">
-									Enter</Link>
-								</button>
-								<button className="btn btn-primary ml-2" type="button" enabled>
-									<span aria-hidden="true" />
-									Sign up
-				 				</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    $('.back').click(function () {
+      $('.signUp').addClass('active-sx')
+      $('.signIn').addClass('inactive-dx')
+      $('.signIn').removeClass('active-dx')
+      $('.signUp').removeClass('inactive-sx')
+    })
+  }
+
+  render () {
+    return (
+      <div className='container' reqired='true'>
+        <form className='signUp' reqired='true'>
+          <h3>Create Your Account</h3>
+          <p>
+            Just enter your email address
+            <br />
+            and your password for join.
+          </p>
+          <input
+            className='w100'
+            type='email'
+            placeholder='Insert eMail'
+            reqired
+            autoComplete='off'
+            reqired='true'
+          />
+          <input type='password' placeholder='Insert Password' reqired />
+          <input type='password' placeholder='Verify Password' reqired />
+          <button className='form-btn sx log-in' type='button'>
+            Log In
+          </button>
+          <button className='form-btn dx' type='submit'>
+            Sign Up
+          </button>
+        </form>
+        <form className='signIn'>
+          <h3>
+            Welcome
+            <br />
+            Back !
+          </h3>
+          <button className='fb' type='button' href='www.facebook.com'>
+            Log In With Facebook
+          </button>
+          <p>- or -</p>
+          <input
+            type='email'
+            placeholder='Insert eMail'
+            autoComplete='off'
+            reqired
+          />
+          <input type='password' placeholder='Insert Password' reqired />
+          <button className='form-btn sx back' type='button'>
+            Back
+          </button>
+          <button className='form-btn dx' type='submit'>
+            Log In
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default Login;
-
+export default Login
