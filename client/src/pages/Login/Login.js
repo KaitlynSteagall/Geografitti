9f64ec8ba1746d5f7ac21929c090ebc7b145edf3
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-12-19 19:24:22 -0800

    functions to login form is working

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,29 +1,30 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import React, { Component } from 'react'
-import './style.css'
-import $ from 'jquery'
+import React, { Component } from 'react';
+import './style.css';
+
 
 class Login extends Component {
-  componentDidMount () {
-    $('.log-in').click(function () {
-      $('.signIn').addClass('active-dx')
-      $('.signUp').addClass('inactive-sx')
-      $('.signUp').removeClass('active-sx')
-      $('.signIn').removeClass('inactive-dx')
-    })
-
-    $('.back').click(function () {
-      $('.signUp').addClass('active-sx')
-      $('.signIn').addClass('inactive-dx')
-      $('.signIn').removeClass('active-dx')
-      $('.signUp').removeClass('inactive-sx')
+  state = { 
+    signIn: "active-dx",
+    signUp: "inactive-sx" 
+  }
+  onLoginClick = () => { 
+    this.setState({ 
+    signIn: "active-dx",
+    signUp: "inactive-sx" 
+  })
+  } 
+  onBackClick = () => {
+    this.setState({
+      signIn: "inactive-dx",
+      signUp: "active-sx" 
     })
   }
 
   render () {
     return (
       <div className='container' reqired='true'>
-        <form className='signUp' reqired='true'>
+        <form className={"signUp " + this.state.signUp} reqired='true'>
           <h3>Create Your Account</h3>
           <p>
             Just enter your email address
@@ -40,20 +41,20 @@ class Login extends Component {
           />
           <input type='password' placeholder='Insert Password' reqired />
           <input type='password' placeholder='Verify Password' reqired />
-          <button className='form-btn sx log-in' type='button'>
+          <button className="form-btn sx log-in " onClick={this.onLoginClick} type='button'>
             Log In
           </button>
           <button className='form-btn dx' type='submit'>
             Sign Up
           </button>
         </form>
-        <form className='signIn'>
+        <form className={"signIn " + this.state.signIn}>
           <h3>
             Welcome
             <br />
             Back !
           </h3>
-          <button className='fb' type='button' href='www.facebook.com'>
+          <button className='fb' type='button' href='https://www.facebook.com'>
             Log In With Facebook
           </button>
           <p>- or -</p>
@@ -64,7 +65,7 @@ class Login extends Component {
             reqired
           />
           <input type='password' placeholder='Insert Password' reqired />
-          <button className='form-btn sx back' type='button'>
+          <button className="form-btn sx back " onClick={this.onBackClick} type='button'>
             Back
           </button>
           <button className='form-btn dx' type='submit'>
@@ -76,4 +77,4 @@ class Login extends Component {
   }
 }
 
-export default Login
+export default Login;
