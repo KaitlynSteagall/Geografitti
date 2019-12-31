cd8ceb0f356832ec173d873b33871fff77750c74
Mars <m.getsoian@gmail.com>
2019-12-31 12:51:05 -0800

    404 page

---
+++ client/src/pages/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+function NotFound() {
+  <body>
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>404</h1>
+          <h2>Page not found</h2>
+        </div>
+        <a href="#">Homepage</a>
+      </div>
+    </div>
+  </body>
+}
+
+export default NotFound;
