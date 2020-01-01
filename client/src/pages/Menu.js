f16cc21f45f135253fc0a703805581a2cd0a15c5
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2019-12-31 16:05:21 -0800

    added transition to logo page

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import MenuButton from "../components/MenuButton/MenuButton";
-import {Link, BrowserRouter as Router } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 
 class Menu extends Component {
 
@@ -9,16 +9,19 @@ class Menu extends Component {
       <div className="container">
         <div className="col-12">
           <div className="row">
-            <MenuButton><Link to="/Take-Photo" style={{color:"white"}}>Make New Art</Link></MenuButton>
+            <MenuButton><Link to="/view-art" style={{ color: "white" }}>Search Art In Your Area</Link></MenuButton>
           </div>
           <div className="row">
-            <MenuButton><Link to="/Portfolio" style={{color:"white"}}>View My Gallery</Link></MenuButton>
+            <MenuButton><Link to="/take-photo" style={{ color: "white" }}>Make New Art</Link></MenuButton>
           </div>
           <div className="row">
-            <MenuButton><Link to="/Edit-Account-Info" style={{color:"white"}}>Edit Account Info</Link></MenuButton>
+            <MenuButton><Link to="/portfolio" style={{ color: "white" }}>View My Gallery</Link></MenuButton>
           </div>
           <div className="row">
-            <MenuButton><Link to="/" style={{color:"white"}}>Logout</Link></MenuButton>
+            <MenuButton><Link to="/edit-account-info" style={{ color: "white" }}>Edit Account Info</Link></MenuButton>
+          </div>
+          <div className="row">
+            <MenuButton><Link to="/" style={{ color: "white" }}>Logout</Link></MenuButton>
           </div>
         </div>
       </div>
