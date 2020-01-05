2c03e181a0683bb874acad5d1185c27677924bd0
Kaitlyn Steagall <51279304+KaitlynSteagall@users.noreply.github.com>
2020-01-05 13:22:59 -0800

    Update index.js

--- client/src/components/ToolButton/index.js
+++ client/src/components/ToolButton/index.js
@@ -1,51 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from "react"; 
-
-=======
-import React from "react";
-import { Tools } from "react-sketch";
->>>>>>> 6b8e95cb41252131df832ed46713fddba1895ec0
-
-function Toolbutton(props) {
-
-<<<<<<< HEAD
-
-
-  function Toolbutton(props){
-
-  
-    return(
-
-      
-      
-      
-      
-    ); 
-  }
-  
-
-
-
-=======
-  return (
-    <button
-      className="btn btn-primary"
-      onClick={props.selectTool}
-      value={Tools.Pencil}>
-      testbutton
-    </button>
-  );
-}
->>>>>>> 6b8e95cb41252131df832ed46713fddba1895ec0
-=======
-
 import React from "react";
 import { Tools } from "react-sketch";
 
-function Toolbutton(props) {
-
-  function Toolbutton(props){
+function Toolbutton(props){
 
   return (
     <button
@@ -56,7 +12,5 @@ function Toolbutton(props) {
     </button>
   );
 }
-
->>>>>>> 658e63f5dd297994b63ea0cc2ec2d466bff59db8
 
 export default Toolbutton; 
