5fcfd0264a3e65353f552f04fbf1e20a17c31211
eazimms <eric.allen.zimmerman@gmail.com>
2020-01-06 18:14:25 -0800

    modal working, some of my tools stopped working, looking into that

--- client/src/components/Canvas/index.js
+++ client/src/components/Canvas/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import { SketchField, Tools } from "react-sketch";
 import { CompactPicker } from "react-color";
 import Photo from "../Camera/index";
-import { Row, Col, Button,MO } from "react-bootstrap";
+import { Row, Col, Button, Modal } from "react-bootstrap";
+
+
 
 class TagArea extends React.Component {
+
+  
   state = {
     lineWidth: 10,
     lineColor: "black",
@@ -36,8 +40,21 @@ class TagArea extends React.Component {
     expandControlled: false,
     text: "a text, cool!",
     enableCopyPaste: false,
-    show: false
+    show: false, 
+    
   };
+
+   showTools = () =>{
+     this.setState({
+       show: true
+     });
+   }; 
+
+   goAway = () => {
+     this.setState({
+       show: false
+     }); 
+   }; 
 
   save = () => {
     let drawings = this.state.drawings;
@@ -57,11 +74,14 @@ class TagArea extends React.Component {
   };
 
   selectTool = event => {
+    console.log("modal reaching here")
     this.setState({
       tool: event.target.value,
       enableRemoveSelected: event.target.value === Tools.Select,
-      enableCopyPaste: event.target.value === Tools.Select
+      enableCopyPaste: event.target.value === Tools.Select,
+      
     });
+    console.log('and now here')
   };
 
   undo = () => {
@@ -151,83 +171,123 @@ class TagArea extends React.Component {
           </Col>
         </Row>
 
+        {/* Photo Modal */}
+
         <Photo
           handlePhotoDataUrl={this.props.handlePhotoDataUrl}
-          onClose={this.something}
+          
         />
 
         <Button variant="secondary" onClick={this.something}>
           Throw it up
         </Button>
 
+        {/* ToolBox Modal */}
+
         
-          <button
+        <Modal show={this.state.show}
+        handleClose={this.goAway}>
+          <Modal.Body>
+          <Button
+          className="btn btn-success"
+          onClick={this.selectTool}
+          value={Tools.Line}
+        >
+          <i class="fas fa-grip-lines"></i>
+        </Button>
+
+        <Button
           className="btn btn-primary"
           onClick={this.selectTool}
           value={Tools.Pencil}
         >
           <i class="fas fa-pencil-alt"></i>
-        </button>
+        </Button>
 
-        <button
-          className="btn btn-success"
+        <Button
+          variant="Secondary"
           onClick={this.selectTool}
-          value={Tools.Line}
+          value={Tools.Circle}
         >
-          Line
-        </button>
+          <i class="far fa-circle"></i>
+        </Button>
 
-        <button
+        <Button
           className="btn btn-primary"
           onClick={this.selectTool}
           value={Tools.Rectangle}
         >
           <i class="fas fa-vector-square"></i>
-        </button>
-        <button
-          className="btn btn-success"
-          onClick={this.selectTool}
-          value={Tools.Pan}
-        >
-          Pan
-        </button>
+        </Button>
 
-        <button
-          className="btn btn-primary"
-          onClick={this.selectTool}
-          value={Tools.Select}
-        >
-          <i class="fas fa-mouse-pointer"></i>
-        </button>
-
-        <button className="btn btn-success" onClick={this.undo}>
-          <i class="fas fa-undo"></i>
-        </button>
-        <button className="btn btn-primary" onClick={this.redo}>
-          <i class="fas fa-redo"></i>
-        </button>
-
-          
-
-        
-
-        <button className="btn btn-success" onClick={this.save}>
-          <i class="far fa-save">Save</i>
-        </button>
-
-        <button
-          id="download"
-          classnName="btn btn-danger"
-          onClick={this.download}
-        >
-          Download
-        </button>
+        <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
 
         <CompactPicker
           color={this.state.lineColor}
           value={this.state.lineColor}
           onChange={color => this.setState({ lineColor: color.hex })}
         />
+
+
+
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.goAway}>Done</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Button onClick={this.showTools}>Open seasame</Button>
+
+        
+
+        
+          
+
+        
+
+       
+        <Button
+          className="btn btn-success"
+          onClick={this.selectTool}
+          value={Tools.Pan}
+        >
+          Pan
+        </Button>
+
+        <Button
+          className="btn btn-primary"
+          onClick={this.selectTool}
+          value={Tools.Select}
+        >
+          <i class="fas fa-mouse-pointer"></i>
+        </Button>
+
+        <Button className="btn btn-success" onClick={this.undo}>
+          <i class="fas fa-undo"></i>
+        </Button>
+        <Button className="btn btn-primary" onClick={this.redo}>
+          <i class="fas fa-redo"></i>
+        </Button>
+        
+
+          
+
+        
+
+        <Button className="btn btn-success" onClick={this.save}>
+          <i class="far fa-save">Save</i>
+        </Button>
+
+        <Button
+          id="download"
+          classnName="btn btn-danger"
+          onClick={this.download}
+        >
+          Download
+        </Button>
+
+        
       </div>
     );
   }
