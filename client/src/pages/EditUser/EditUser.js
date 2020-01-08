43f815c708e764fbc66e4c99dccd0250d4d75349
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2020-01-07 16:43:29 -0800

    style edits to edit user page

--- client/src/pages/EditUser/EditUser.js
+++ client/src/pages/EditUser/EditUser.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import "../EditUser/EditUser.css"
 
 class EditUser extends React.Component {
   state = {
@@ -25,70 +26,70 @@ class EditUser extends React.Component {
           style={{
             position: 'fixed',
             background: 'rgba(255, 125, 0, 0.72)',
-            width: '100%',
-            height: '100%',
+            width: '100vw',
+            height: '100vh',
             top: 0,
             left: 0
           }}
         ></div>
-        <form>
-          <div className='form-group'>
-            <label>Current Username</label>
+        <form className="ml-1 m-0">
+          <div className='form-group form-group-sm m-0'>
+            <label className='topLabel'>Current Username</label>
             <input
               value={this.state.currentUser}
               onChange={this.handleEmailChange}
               type='username'
-              className='form-control'
+              className='form-control input-sm m-0'
               id='exampleInputUser1'
               aria-describedby='userHelp'
-              placeholder='Current Username'
+              // placeholder='Current Username'
             />
           </div>
-          <div className='form-group'>
+          <div className='form-group form-group-sm m-0'>
             <label>New Username</label>
             <input
               value={this.state.newUserName}
               onChange={this.handleNameChange}
-              type='username'
-              className='form-control'
+              type='Username'
+              className='form-control input-sm m-0'
               id='exampleInputUsername1'
-              placeholder='username'
+              // placeholder='Username'
             />
           </div>
-          <div className='form-group'>
+          <div className='form-group form-group-sm m-0'>
             <label>Current Password</label>
             <input
               value={this.state.userPassword}
               onChange={this.handlePasswordChange}
               type='password'
-              className='form-control'
+              className='form-control input-sm m-0'
               id='exampleInputPassword1'
-              placeholder='Password'
+              // placeholder='Password'
             />
           </div>
-          <div className='form-group'>
+          <div className='form-group form-group-sm m-0'>
             <label>New Password</label>
             <input
               value={this.state.userPassword}
               onChange={this.handlePasswordChange}
               type='password'
-              className='form-control'
+              className='form-control input-sm m-0'
               id='exampleInputPassword1'
-              placeholder='Password'
+              // placeholder='Password'
             />
           </div>
-          <div className='form-group'>
+          <div className='form-group form-group-sm m-0'>
             <label>Confirm New Password</label>
             <input
               value={this.state.userPassword}
               onChange={this.handlePasswordChange}
               type='password'
-              className='form-control'
+              className='form-control input-sm m-0 pb-2'
               id='exampleInputPassword1'
-              placeholder='Password'
+              // placeholder='Password'
             />
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button type='submit' className='btn mt-3'>
             Submit
           </button>
         </form>
